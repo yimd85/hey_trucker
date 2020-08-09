d5cdd10d81661a3e7b9279bcec00d48fea06e8d4
unknown <yimd85@gmail.com>
2020-08-09 11:28:19 -0400

    updated google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -17,25 +17,27 @@ passport.deserializeUser((id, done) => {
         });
 });
 
-passport.use(new GoogleStrategy({
-    clientID: keys.GOOGLE_CLIENT_ID, //variable is located in ./config/keys
-    clientSecret: keys.GOOGLE_CLIENT_SECRET, //variable is located in ./config/keys
-    callbackURL: "/auth/google/callback"
-},
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then(existingUser => {
-                //if the User collection has a record w/ googleId that exists, do not save
-                if (existingUser) {
-                    //done is a method from passport that lets the auth case know that it is finished
-                    done(null, existingUser);
-                } else {
-                    //create a new user and save to database
-                    //create a new model instance
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(userNew => done(null, userNew));
-                }
-            });
-    }
-));
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.GOOGLE_CLIENT_ID, //variable is located in ./config/keys
+        clientSecret: keys.GOOGLE_CLIENT_SECRET, //variable is located in ./config/keys
+        callbackURL: "/auth/google/callback",
+        proxy: true
+    },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ googleId: profile.id })
+                .then(existingUser => {
+                    //if the User collection has a record w/ googleId that exists, do not save
+                    if (existingUser) {
+                        //done is a method from passport that lets the auth case know that it is finished
+                        done(null, existingUser);
+                    } else {
+                        //create a new user and save to database
+                        //create a new model instance
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(userNew => done(null, userNew));
+                    }
+                });
+        }
+    ));

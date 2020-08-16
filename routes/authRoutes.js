c426495889818e73b95b91bb42bb79bb52bdbbfe
unknown <yimd85@gmail.com>
2020-08-16 00:41:44 -0400

    removed test route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,9 +2,9 @@ const passport = require('passport');
 
 module.exports = (app) => {
 
-    app.get('/', (req, res) => {
-        res.send({ bye: 'buddy' });
-    });
+    // app.get('/', (req, res) => {
+    //     res.send({ bye: 'buddy' });
+    // });
 
     app.get('/auth/google',
         passport.authenticate('google', { scope: ['profile', 'email'] }));

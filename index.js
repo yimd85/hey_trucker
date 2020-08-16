3e2d40e106c50b2de8cf5fe81bdbe57143945360
unknown <yimd85@gmail.com>
2020-08-16 00:38:11 -0400

    deployment updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport')
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/User'); //this needs to be first because this will execute
@@ -29,6 +29,22 @@ app.use(passport.session());
 //authRoutes(app);
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+//this is code for production only
+if (process.env.NODE_ENV === 'production') {
+    //make sure that express will serve up production assets files (like main.js or main.css files)
+    app.use(express.static('client/build')); //if there is a request first match up to see if any files match up with the incoming request. This must always be first
+    //if the request matchs with the files in the above express will answer it with the files in the above. 
+
+
+    //make sure that exoress will serve up the index.html if express does not recognize the route
+    const path = require('path');
+    //this line says, if the request is not in the express routes handlers, no files in our client then provide the index.html file.
+    //this is the catch all for all requests
+    app.get('*', (req, res) => { 
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
